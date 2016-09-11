ca512c50cde78fbf0a5bb56299050af6c03c63e5
ftdsxo <ftdsxo@https://github.com/ftdjsxo/restTemplate>
2016-09-11 17:01:59 +0200

    Initial Commit

---
+++ Factory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Factory
+//
+//  Created by Francesco Thiery on 11/09/16.
+//  Copyright © 2016 cookedhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
